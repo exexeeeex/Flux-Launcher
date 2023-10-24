552f256d3d47b2ce5823fbd1e0272ede3a1dedbb
feel <90983538+exexeeeex@users.noreply.github.com>
2023-10-24 19:09:03 +0300

    Update get.py

--- get.py
+++ get.py
@@ -1,3 +1,3 @@
 import webbrowser
 
-webbrowser.open_new_tab("https://mega.nz/file/aN5mzBgQ#An4cLq5zknPbWJzcvY0r6ngn0OVRn_b1Slp8jnlGZWc")
+webbrowser.open_new_tab("https://mega.nz/file/PAYlWLLS#FNVYarx8zKUamivt8GC2-f9X5nsKxCgSWhZFF_0hjpQ")
